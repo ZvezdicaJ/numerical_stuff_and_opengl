efb77cc868292efde3d680ed59ad06134cf75953
Jure Lapajne <lapajne.jure@gmail.com>
2018-12-07 13:55:01 +0100

    update

---
+++ geometrical_objects/testing/auxliary_functions_test.cpp
@@ -0,0 +1,9 @@
+#include "standard_includes.hpp"
+#include "auxiliary_functions.hpp"
+
+
+int main(){
+
+
+
+}
---
+++ geometrical_objects/testing/standard_includes.hpp
@@ -0,0 +1,14 @@
+#include <xmmintrin.h>
+#include <algorithm>
+#include <array>
+#include <cmath>
+#include <ctime>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <iostream>
+#include <limits>
+#include <string>
+#include <type_traits>
+#include <unordered_map>
+#include <vector>
