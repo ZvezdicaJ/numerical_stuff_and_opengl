1e45cc9bc18e6f1d76981fca7f99d088aa762a9f
Jure Lapajne <lapajne.jure@gmail.com>
2019-01-24 14:30:01 +0100

    star3d in progress

--- geometrical_objects/src/star3d.hpp
+++ geometrical_objects/src/star3d.hpp
@@ -1,8 +1,9 @@
-#ifndef __STAR3D__
+#ifndef __STAR3D__x
 
 /**
    @class Star3d
-   @brief this class contains vertexes and elements for 3 dimensional star shape.
+   @brief this class contains vertexes and elements for 3 dimensional star
+   shape.
  */
 template <typename T = float> class Star3d : public Shape3D<T> {
   private:
@@ -10,24 +11,13 @@ template <typename T = float> class Star3d : public Shape3D<T> {
 
   public:
     Star3d();
-    Star3d(int, T = 0.5);
+    Star3d(int = 5, T = 0.5);
     Star3d(Star3d &&) = default;
     Star3d &operator=(Star3d &&) = default;
     Star3d(const Star3d &) = default;
     Star3d &operator=(const Star3d &) = default;
     T perimeter();
     T ratio;
-};
-
-template <typename T> Star3d<T>::Star3d() {
-    this->draw_type = 'V';
-    this->vertex_size = 3;
-    this->min_vertexes = 2;
-    this->generate_vertexes();
-    this->initialize_buffers();
-    this->generate_filling_ebo();
-    // std::cout << "circle buffers:\nVBO: " << this->VBO << "\nVAO: " <<
-    // this->VAO << std::endl;
 };
 
 template <typename T> Star3d<T>::Star3d(int bulges, T ratio_) : ratio(ratio_) {
@@ -52,10 +42,9 @@ inline void Star3d<T>::generate_vertexes(int bulges, T ratio) {
     this->vertexes.resize(2 * 3 *
                           bulges); // 2 because to each leg owns 2 points and 3
                                    // because each point has 3 coordinates
-    int tocke = 2 * bulges;
-    int r = 4 * ((tocke) / 4); // points should be a multiple of 4
-    int reminder = tocke - r;  // stevilo preostalih tock
 
+    this->element_array.reserve(12 * bulges);
+    this->element_array.resize(12 * bulges);
     // first number is number of steps in fi direction.
     // second number is number of steps in theta direction.
     std::pair<int, int> steps = closest_pair(find_products(bulges));
@@ -63,7 +52,7 @@ inline void Star3d<T>::generate_vertexes(int bulges, T ratio) {
     float korak_fi = M_PI / steps.first;
     float korak_c_theta = 2.0 / steps.second;
 
-    int index;
+    int index = 0;
 
     for (int j = 0; j < steps.second; j++) {
         T ct = -1 + korak_c_theta;
@@ -78,8 +67,34 @@ inline void Star3d<T>::generate_vertexes(int bulges, T ratio) {
             index += 3;
         }
     }
-}
 
+    for (int j = 0; j < steps.second - 1; j++) {
+        T ct = -1 + korak_c_theta + korak_theta / 2;
+        T st = sqrt(1 - ct * ct);
+        for (int i = 0; i < 2 * steps.first - 1; i++) {
+            T fi = i * korak_fi + korak_fi / 2;
+            T cf = cos(fi);
+            T sf = sin(fi);
+            vertexes[index] = st * cf;
+            vertexes[index + 1] = st * sf;
+            vertexes[index + 2] = ct;
+            index += 3;
+        }
+    }
+    // set elements
+    int column = 0;
+    int row = 0;
+    int mid_point_start = steps.first * steps.second;
+    int triangles = 0;
+    for (int i = 0; i < bulges; i++) {
+        int mid_point = mid_point_start + (steps.first - 1) * row;
+        int p1 = column + row * steps.first;
+        int p2 = column + row * steps.first + 1;
+        int p3 = column + (row + 1) * steps.first;
+        int p4 = column + (row + 1) * steps.first + 1;
+        element_array[triangle]=
+    }
+}
 
 #define __STAR3D__
 #endif
