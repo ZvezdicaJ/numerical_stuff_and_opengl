936c58e43d26120c418d81ab7dd07368935b94ff
Jure Lapajne <lapajne.jure@gmail.com>
2019-02-02 15:28:16 +0100

    bitonic sort for two float vectors finished and tested - it works!

--- geometrical_objects/src/sort.hpp
+++ geometrical_objects/src/sort.hpp
@@ -4,7 +4,7 @@
  *@brief The function accepts a single __m256d vector and sorts it.
  * @param reg register to be sorted
  */
-void bitonic_avx_reg_sort(__m256 &reg) {
+void bitonic_sort(__m256 &reg) {
     {
         // print_avx(reg, "reg: ");
         // prvi korak: *--*  *--*  *--* *--*
@@ -65,7 +65,7 @@ void bitonic_avx_reg_sort(__m256 &reg) {
     }
     // print_avx(reg, "reg: ");
     {
-        // repeat second step  *----* *--*     *----* *--*
+        //  *----* *----*     *----* *----*
         __m256 shuffled_reg = _mm256_shuffle_ps(reg, reg, 0b01001110);
         __m256 max = _mm256_max_ps(reg, shuffled_reg);
         __m256 min = _mm256_min_ps(reg, shuffled_reg);
@@ -89,13 +89,110 @@ void bitonic_avx_reg_sort(__m256 &reg) {
 }
 #endif
 
+/**
+ * @brief The function accepts two __m256d vectors and sorts them.
+ * @param reg1 upper vector of numbers - at the end it contains larger values,
+ * the largest value is in the upper half of register [255:192]
+ *
+ * @param reg0 lower vector of numbers - at the end it contains smaller values.
+ * The smallest value is in the lowest half of register - at [63:0]
+ *
+ */
+
+inline void bitonic_sort(__m256 &reg0, __m256 &reg1) {
+    bitonic_sort(reg1); // sort first register
+    bitonic_sort(reg0); // sort second register
+    // print_avx(reg0, "reg0: ");
+    // print_avx(reg1, "reg1: ");
+    // std::cout << std::endl;
+    {
+        // reverse one of registers register reg0
+        __m256 reversed_halves = _mm256_permute2f128_ps(reg0, reg0, 0b00000001);
+        __m256 reversed =
+            _mm256_shuffle_ps(reversed_halves, reversed_halves, 0b00011011);
+
+        // register 2 vsebuje min vrednosti
+        reg0 = _mm256_min_ps(reg1, reversed);
+        // register 1 vsebuje max vrednosti
+        reg1 = _mm256_max_ps(reg1, reversed);
+        // print_avx(max, "max: ");
+        // print_avx(min, "min: ");
+    }
+    // print_avx(reg0, "reg0: ");
+    // print_avx(reg1, "reg1: ");
+    // std::cout << std::endl;
+    {
+        // tu narediš *----* *----* *----* *----*
+        __m256 reversed_halves = _mm256_permute2f128_ps(reg0, reg0, 0b00000001);
+        __m256 max = _mm256_max_ps(reg0, reversed_halves);
+        __m256 min = _mm256_min_ps(reg0, reversed_halves);
+        // max mora biti pri 256
+        reg0 = _mm256_blend_ps(max, min, 0b00001111);
+    }
+    {
+        // tu narediš *----* *----* *----* *----*
+        __m256 reversed_halves = _mm256_permute2f128_ps(reg1, reg1, 0b00000001);
+        __m256 max = _mm256_max_ps(reg1, reversed_halves);
+        __m256 min = _mm256_min_ps(reg1, reversed_halves);
+        // max mora biti pri 256
+        reg1 = _mm256_blend_ps(max, min, 0b00001111);
+    }
+
+    {
+        //  *----* *----*     *----* *----*
+        __m256 shuffled_reg = _mm256_shuffle_ps(reg0, reg0, 0b01001110);
+        __m256 max = _mm256_max_ps(reg0, shuffled_reg);
+        __m256 min = _mm256_min_ps(reg0, shuffled_reg);
+        // max mora biti pri 256
+        // min is located at the start of register (at 0 - lower half)
+        reg0 = _mm256_blend_ps(max, min, 0b00110011);
+    }
+    // print_avx(reg, "reg: ");
+    {
+        // and finally repeat the first step: *--*  *--*  *--* *--*
+        __m256 shuffled_reg = _mm256_shuffle_ps(reg0, reg0, 0b10110001);
+        __m256 max = _mm256_max_ps(reg0, shuffled_reg);
+        __m256 min = _mm256_min_ps(reg0, shuffled_reg);
+        //   dst[63:0] := src1[31:0]
+        //   dst[127:64] := src2[63:32]
+        //   dst[192:128] := src1[95:64]
+        //   dst[255:192] := src2[127:96]
+        // this will produce smallest number to in the [0:63] register
+        reg0 = _mm256_blend_ps(min, max, 0b10101010);
+    }
+
+    {
+        //  *----* *----*     *----* *----*
+        __m256 shuffled_reg = _mm256_shuffle_ps(reg1, reg1, 0b01001110);
+        __m256 max = _mm256_max_ps(reg1, shuffled_reg);
+        __m256 min = _mm256_min_ps(reg1, shuffled_reg);
+        // max mora biti pri 256
+        // min is located at the start of register (at 0 - lower half)
+        reg1 = _mm256_blend_ps(max, min, 0b00110011);
+    }
+    // print_avx(reg, "reg: ");
+    {
+        // and finally repeat the first step: *--*  *--*  *--* *--*
+        __m256 shuffled_reg = _mm256_shuffle_ps(reg1, reg1, 0b10110001);
+        __m256 max = _mm256_max_ps(reg1, shuffled_reg);
+        __m256 min = _mm256_min_ps(reg1, shuffled_reg);
+        //   dst[63:0] := src1[31:0]
+        //   dst[127:64] := src2[63:32]
+        //   dst[192:128] := src1[95:64]
+        //   dst[255:192] := src2[127:96]
+        // this will produce smallest number to in the [0:63] register
+        reg1 = _mm256_blend_ps(min, max, 0b10101010);
+    }
+    return;
+}
+
 #ifdef __AVX2__
 #define __BITONIC_SORT_DOUBLE__
 /**
  *@brief The function accepts a single __m256d vector and sorts it.
  * @param reg register to be sorted
  */
-inline void bitonic_avx_reg_sort(__m256d &reg) {
+inline void bitonic_sort(__m256d &reg) {
     {
         __m256d shuffled_reg = _mm256_shuffle_pd(reg, reg, 0b0101);
         __m256d max = _mm256_max_pd(reg, shuffled_reg);
@@ -130,30 +227,32 @@ inline void bitonic_avx_reg_sort(__m256d &reg) {
 
 /**
  * @brief The function accepts two __m256d vectors and sorts them.
- * @param reg1 upper vector of numbers - at the end it contains larger values
- * @param reg0 lower vector of numbers - at the end it contains smaller values
+ * @param reg1 upper vector of numbers - at the end it contains larger values,
+ * the largest value is in the upper half of register [255:192]
+ *
+ * @param reg0 lower vector of numbers - at the end it contains smaller values.
+ * The smallest value is in the lowest half of register - at [63:0]
+ *
  */
-inline void bitonic_2reg_sort(__m256d &reg0, __m256d &reg1) {
-    bitonic_avx_reg_sort(reg1); // sort first register
-    bitonic_avx_reg_sort(reg0); // sort second register
-    //print_avx(reg0, "reg0: ");
-    //print_avx(reg1, "reg1: ");
-    //std::cout << std::endl;
+inline void bitonic_sort(__m256d &reg0, __m256d &reg1) {
+    bitonic_sort(reg1); // sort first register
+    bitonic_sort(reg0); // sort second register
+    // print_avx(reg0, "reg0: ");
+    // print_avx(reg1, "reg1: ");
+    // std::cout << std::endl;
     {
         // reverse one of registers register reg0
         __m256d reverse2 = _mm256_permute4x64_pd(reg0, _MM_SHUFFLE(0, 1, 2, 3));
-        __m256d max = _mm256_max_pd(reg1, reverse2);
-        __m256d min = _mm256_min_pd(reg1, reverse2);
+        // register 2 vsebuje min vrednosti
+        reg0 = _mm256_min_pd(reg1, reverse2);
+        // register 1 vsebuje max vrednosti
+        reg1 = _mm256_max_pd(reg1, reverse2);
         // print_avx(max, "max: ");
         // print_avx(min, "min: ");
-        // register 1 vsebuje max vrednosti
-        reg1 = _mm256_blend_pd(max, min, 0b0000);
-        // register 2 vsebuje min vrednosti
-        reg0 = _mm256_blend_pd(max, min, 0b1111);
     }
-    //print_avx(reg0, "reg0: ");
-    //print_avx(reg1, "reg1: ");
-    //std::cout << std::endl;
+    // print_avx(reg0, "reg0: ");
+    // print_avx(reg1, "reg1: ");
+    // std::cout << std::endl;
     {
         // tu narediš *----* *----*
         __m256d shuffled_reg1 = _mm256_permute4x64_pd(reg1, 0b01001110);
@@ -162,9 +261,9 @@ inline void bitonic_2reg_sort(__m256d &reg0, __m256d &reg1) {
         // max mora biti pri 256
         reg1 = _mm256_blend_pd(max, min, 0b0011);
     }
-    //print_avx(reg0, "reg0: ");
-    //print_avx(reg1, "reg1: ");
-    //std::cout << std::endl;
+    // print_avx(reg0, "reg0: ");
+    // print_avx(reg1, "reg1: ");
+    // std::cout << std::endl;
     {
         __m256d shuffled_reg1 = _mm256_shuffle_pd(reg1, reg1, 0b0101);
         __m256d max = _mm256_max_pd(reg1, shuffled_reg1);
@@ -176,9 +275,9 @@ inline void bitonic_2reg_sort(__m256d &reg0, __m256d &reg1) {
         // this will produce smallest number to in the [0:63] register
         reg1 = _mm256_unpacklo_pd(min, max);
     }
-    //print_avx(reg0, "reg0: ");
-    //print_avx(reg1, "reg1: ");
-    //std::cout << std::endl;
+    // print_avx(reg0, "reg0: ");
+    // print_avx(reg1, "reg1: ");
+    // std::cout << std::endl;
     {
         // tu narediš *----* *----*
         __m256d shuffled_reg0 = _mm256_permute4x64_pd(reg0, 0b01001110);
@@ -187,9 +286,9 @@ inline void bitonic_2reg_sort(__m256d &reg0, __m256d &reg1) {
         // max mora biti pri 256
         reg0 = _mm256_blend_pd(max, min, 0b0011);
     }
-    //print_avx(reg0, "reg0: ");
-    //print_avx(reg1, "reg1: ");
-    //std::cout << std::endl;
+    // print_avx(reg0, "reg0: ");
+    // print_avx(reg1, "reg1: ");
+    // std::cout << std::endl;
     {
         __m256d shuffled_reg0 = _mm256_shuffle_pd(reg0, reg0, 0b0101);
         __m256d max = _mm256_max_pd(reg0, shuffled_reg0);
@@ -203,7 +302,7 @@ inline void bitonic_2reg_sort(__m256d &reg0, __m256d &reg1) {
     }
     // print_avx(reg0, "reg0: ");
     // print_avx(reg1, "reg1: ");
-    //std::cout << std::endl;
+    // std::cout << std::endl;
     return;
 }
 
@@ -217,17 +316,17 @@ inline void bitonic_2reg_sort(__m256d &reg0, __m256d &reg1) {
  * @param first_index is the starting index of element to be sorted
  * @param last_index is the last index of element to be sorted
  */
-inline void bitonic_n_sort(aligned_vector<__m256d> &full_vec,
-                           unsigned first_index = 0, unsigned last_index = 0) {
+inline void bitonic_sort(aligned_vector<__m256d> &full_vec,
+                         unsigned first_index = 0, unsigned last_index = 0) {
     if (last_index == 0)
         last_index = full_vec.size() - 1;
 
     unsigned num_to_sort = 1 + last_index - first_index;
     if (num_to_sort == 1)
-        bitonic_avx_reg_sort(full_vec[first_index]);
+        bitonic_sort(full_vec[first_index]);
 
     else if (num_to_sort == 2)
-        bitonic_2reg_sort(full_vec[first_index], full_vec[last_index]);
+        bitonic_sort(full_vec[first_index], full_vec[last_index]);
 
     else if (num_to_sort % 2 == 0) {
         for (unsigned i = 0; i < num_to_sort / 2; i++) {
@@ -240,9 +339,9 @@ inline void bitonic_n_sort(aligned_vector<__m256d> &full_vec,
             // register 2 vsebuje min vrednosti
             full_vec[first_index + i] = _mm256_blend_pd(max, min, 0b1111);
         }
-        bitonic_n_sort(full_vec, first_index + num_to_sort / 2 - 1,
-                       first_index + num_to_sort / 2);
-        bitonic_n_sort(full_vec, first_index + num_to_sort / 2, last_index);
+        bitonic_sort(full_vec, first_index + num_to_sort / 2 - 1,
+                     first_index + num_to_sort / 2);
+        bitonic_sort(full_vec, first_index + num_to_sort / 2, last_index);
     }
     // temporary solution until
     else {
@@ -260,9 +359,9 @@ inline void bitonic_n_sort(aligned_vector<__m256d> &full_vec,
             full_vec[first_index + i] = _mm256_blend_pd(max, min, 0b1111);
         }
 
-        bitonic_n_sort(full_vec, first_index + num_to_sort / 2 - 1,
-                       first_index + num_to_sort / 2);
-        bitonic_n_sort(full_vec, first_index + num_to_sort / 2, last_index);
+        bitonic_sort(full_vec, first_index + num_to_sort / 2 - 1,
+                     first_index + num_to_sort / 2);
+        bitonic_sort(full_vec, first_index + num_to_sort / 2, last_index);
     }
 }
 #endif
--- geometrical_objects/testing/sort_test.hpp
+++ geometrical_objects/testing/sort_test.hpp
@@ -9,7 +9,7 @@ auto random_double = std::bind(double_dist, generator);
 TEST(sort, bitonic_avx_reg_sort_double) {
     {
         __m256d reg = _mm256_setr_pd(0, 1, 2, 3);
-        bitonic_avx_reg_sort(reg);
+        bitonic_sort(reg);
         double *sol = (double *)&reg;
         double solution[4] = {0, 1, 2, 3};
         for (int i = 0; i < 4; i++)
@@ -17,7 +17,7 @@ TEST(sort, bitonic_avx_reg_sort_double) {
     }
     {
         __m256d reg2 = _mm256_setr_pd(-10, 3, 6, -12);
-        bitonic_avx_reg_sort(reg2);
+        bitonic_sort(reg2);
         double *sol2 = (double *)&reg2;
         double solution2[4] = {-12, -10, 3, 6};
         for (int i = 0; i < 4; i++)
@@ -25,7 +25,7 @@ TEST(sort, bitonic_avx_reg_sort_double) {
     }
     {
         __m256d reg3 = _mm256_setr_pd(5, 3, 6, 48);
-        bitonic_avx_reg_sort(reg3);
+        bitonic_sort(reg3);
         double *sol3 = (double *)&reg3;
         double solution3[4] = {3, 5, 6, 48};
         for (int i = 0; i < 4; i++)
@@ -38,7 +38,7 @@ TEST(sort, bitonic_avx_reg_sort_double) {
         __m256d reg0 = _mm256_loadu_pd(inp0.data());
         std::sort(std::begin(inp0), std::end(inp0));
 
-        bitonic_avx_reg_sort(reg0);
+        bitonic_sort(reg0);
         double *s1 = (double *)&reg0;
         // std::cout << "inp: " << std::endl;
         for (int i = 0; i < 4; i++) {
@@ -51,7 +51,7 @@ TEST(sort, bitonic_avx_sort_2reg) {
     {
         __m256d reg0 = _mm256_setr_pd(1, 2, 3, 4);
         __m256d reg1 = _mm256_setr_pd(5, 6, 7, 8);
-        bitonic_2reg_sort(reg0, reg1);
+        bitonic_sort(reg0, reg1);
         double *s1 = (double *)&reg0;
         double *s2 = (double *)&reg1;
         double sol1[4] = {1, 2, 3, 4};
@@ -64,7 +64,7 @@ TEST(sort, bitonic_avx_sort_2reg) {
     {
         __m256d reg0 = _mm256_setr_pd(-1, -2, 18, 29);
         __m256d reg1 = _mm256_setr_pd(500, -80, 2, -5);
-        bitonic_2reg_sort(reg0, reg1);
+        bitonic_sort(reg0, reg1);
         double *s1 = (double *)&reg0;
         double *s2 = (double *)&reg1;
         double sol1[4] = {-80, -5, -2, -1};
@@ -86,7 +86,7 @@ TEST(sort, bitonic_avx_sort_2reg) {
         inp0.insert(inp0.end(), inp1.begin(), inp1.end());
         std::sort(std::begin(inp0), std::end(inp0));
 
-        bitonic_2reg_sort(reg0, reg1);
+        bitonic_sort(reg0, reg1);
         double *s1 = (double *)&reg0;
         double *s2 = (double *)&reg1;
         // std::cout << "inp: " << std::endl;
@@ -101,7 +101,7 @@ TEST(sort, bitonic_avx_reg_sort_float) {
 
     {
         __m256 reg = _mm256_setr_ps(0, 1, 2, 3, 4, 5, 6, 7);
-        bitonic_avx_reg_sort(reg);
+        bitonic_sort(reg);
         float *sol = (float *)&reg;
         float solution[8] = {0, 1, 2, 3, 4, 5, 6, 7};
         for (int i = 0; i < 8; i++)
@@ -109,7 +109,7 @@ TEST(sort, bitonic_avx_reg_sort_float) {
     }
     {
         __m256 reg2 = _mm256_setr_ps(1, 5, 8, 3, 9, 8, 10, -2);
-        bitonic_avx_reg_sort(reg2);
+        bitonic_sort(reg2);
         // print_avx(reg2, "reg2: ");
         float *sol2 = (float *)&reg2;
         float solution2[8] = {-2, 1, 3, 5, 8, 8, 9, 10};
@@ -118,7 +118,7 @@ TEST(sort, bitonic_avx_reg_sort_float) {
     }
     {
         __m256 reg3 = _mm256_setr_ps(100, 5, -8, 3, -9, 15, 0, -2);
-        bitonic_avx_reg_sort(reg3);
+        bitonic_sort(reg3);
         // print_avx(reg2, "reg2: ");
         float *sol3 = (float *)&reg3;
         float solution3[8] = {-9, -8, -2, 0, 3, 5, 15, 100};
@@ -134,11 +134,64 @@ TEST(sort, bitonic_avx_reg_sort_float) {
 
         std::sort(std::begin(inp0), std::end(inp0));
 
-        bitonic_avx_reg_sort(reg0);
+        bitonic_sort(reg0);
         float *s1 = (float *)&reg0;
         // std::cout << "inp: " << std::endl;
         for (int i = 0; i < 8; i++) {
             ASSERT_EQ(*(s1 + i), inp0[i]);
+        }
+    }
+}
+
+TEST(sort, bitonic_avx_sort_float_two_vectors) {
+    {
+        __m256 reg0 = _mm256_setr_ps(1, 2, 3, 4, 5, 6, 7, 8);
+        __m256 reg1 = _mm256_setr_ps(9, 10, 11, 12, 13, 14, 15, 16);
+        bitonic_sort(reg0, reg1);
+        float *s1 = (float *)&reg0;
+        float *s2 = (float *)&reg1;
+        float sol1[8] = {1, 2, 3, 4, 5, 6, 7, 8};
+        float sol2[8] = {9, 10, 11, 12, 13, 14, 15, 16};
+        for (int i = 0; i < 8; i++) {
+            ASSERT_EQ(*(s1 + i), sol1[i]);
+            ASSERT_EQ(*(s2 + i), sol2[i]);
+        }
+    }
+    {
+        __m256 reg0 = _mm256_setr_ps(-1, -2, 18, 29, 48, 52, -50, -22);
+        __m256 reg1 = _mm256_setr_ps(500, -80, 2, -5, 100, 0, 1, 5);
+        bitonic_sort(reg0, reg1);
+        float *s1 = (float *)&reg0;
+        float *s2 = (float *)&reg1;
+        float sol1[8] = {-80, -50, -22, -5, -2, -1, 0, 1};
+        float sol2[8] = {2, 5, 18, 29, 48, 52, 100, 500};
+        for (int i = 0; i < 8; i++) {
+            ASSERT_EQ(*(s1 + i), sol1[i]);
+            ASSERT_EQ(*(s2 + i), sol2[i]);
+        }
+    }
+    
+    {
+        std::vector<float> inp0({random_float(), random_float(), random_float(),
+                                 random_float(), random_float(), random_float(),
+                                 random_float(), random_float()});
+        std::vector<float> inp1({random_float(), random_float(), random_float(),
+                                 random_float(), random_float(), random_float(),
+                                 random_float(), random_float()});
+
+        __m256 reg0 = _mm256_loadu_ps(inp0.data());
+        __m256 reg1 = _mm256_loadu_ps(inp1.data());
+
+        inp0.insert(inp0.end(), inp1.begin(), inp1.end());
+        std::sort(std::begin(inp0), std::end(inp0));
+
+        bitonic_sort(reg0, reg1);
+        float *s1 = (float *)&reg0;
+        float *s2 = (float *)&reg1;
+        // std::cout << "inp: " << std::endl;
+        for (int i = 0; i < 8; i++) {
+            ASSERT_EQ(*(s1 + i), inp0[i]);
+            ASSERT_EQ(*(s2 + i), inp0[i + 8]);
         }
     }
 }
