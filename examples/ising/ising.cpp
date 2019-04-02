b0b3a4257b616c209e01baf3dade1e1409cc08cf
Jure Lapajne <lapajne.jure@gmail.com>
2019-04-02 11:14:52 +0200

    ising fixed

--- examples/ising/ising.cpp
+++ examples/ising/ising.cpp
@@ -20,18 +20,26 @@
 
 int main() {
     IsingModel<float> alg1(102);
-    alg1.set_temperature(0.5);
+    alg1.set_temperature(2.7);
+
+    alg1.set_all_spins_up();
+    std::cout << "magnetization: " << alg1.calc_magnetization()
+              << "\nenergy: " << alg1.calc_energy() << std::endl;
+
     for (int i = 0; i < 1000; i++) {
         std::cout << "magnetization: " << alg1.calc_magnetization()
-                  << "  cluster size: " << alg1.get_cluster_size() << std::endl;
+                  << "\ncluster size: " << alg1.get_cluster_size()
+                  << "\nenergy: " << alg1.calc_energy() << std::endl;
         alg1.flip_cluster();
+        std::cout << "\n\n" << std::endl;
     }
     alg1.set_temperature(5);
     std::cout << "\n*****temperature increased*****\n" << std::endl;
     for (int i = 0; i < 2000; i++) {
         alg1.flip_cluster();
         std::cout << "magnetization: " << alg1.calc_magnetization()
-                  << "  cluster size: " << alg1.get_cluster_size() << std::endl;
+                  << "  cluster size: " << alg1.get_cluster_size() << "\n"
+                  << std::endl;
     }
 
     alg1.metropolis_steps(1000);
